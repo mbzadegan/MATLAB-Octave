932012a4e2f98c8b50674484c8af7e271ec63f7f
Mohammad BadieZadegan <mbzadegan@gmail.com>
2024-10-21 14:42:16 -0700

    Create Prime_Fibo.m

---
+++ Prime_Fibo.m
@@ -0,0 +1,21 @@
+% MATLAB code to find and display prime Fibonacci numbers
+
+% Initialize the first two Fibonacci numbers
+fib(1) = 0;
+fib(2) = 1;
+
+% Number of Fibonacci numbers to generate (you can increase this)
+numFib = 50;  % Adjust to get more Fibonacci numbers
+
+% Generate Fibonacci numbers
+for i = 3:numFib
+    fib(i) = fib(i-1) + fib(i-2);
+end
+
+% Display prime Fibonacci numbers
+fprintf('Prime Fibonacci Numbers:\n');
+for i = 1:numFib
+    if isprime(fib(i)) && fib(i) > 1
+        fprintf('%d\n', fib(i));
+    end
+end
